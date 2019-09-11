c8845840c5b302a5b6d7aa51170047c81d865e74
Karolina <Karolina.Dudek@academytrainee.com>
2019-09-11 13:53:04 +0100

    add dice

--- FrontEnd/scripts/indexpage.js
+++ FrontEnd/scripts/indexpage.js
@@ -24,6 +24,7 @@ number.onkeydown = function(keys){
     }
 }
 
+// Connection request to paste existing data into table
 function makeRequest(){
     return new Promise((resolve,reject)=>{
         const xhr = new XMLHttpRequest();
@@ -59,4 +60,37 @@ makeRequest()
         console.log("It Failed",error);
     });
 
-    
+// Add new data through form
+function addDice(){
+    return new Promise((resolve,reject)=>{
+        const xhr = new XMLHttpRequest();
+        xhr.onload = () => {
+            if (xhr.status ==200) {
+                resolve(xhr.response);} 
+            else {reject("Request Failed");}
+        };
+        xhr.open("POST","http://localhost:9000/dice");
+        xhr.send();
+    });
+}
+addDice()
+    .then((data)=>{
+        console.log("It Worked",data);
+        let parsedData = JSON.parse(data);
+        for(item of parsedData){
+            console.log(item);
+            let tabRow = document.createElement("tr");
+            for(key in item){
+                if(item.hasOwnProperty(key)){
+                    let tabData = document.createElement("td");
+                    tabData.innerText = item[key];
+                    console.log(item[key]);
+                    tabRow.appendChild(tabData);
+                }
+            }
+            document.getElementById("dice_table").appendChild(tabRow);
+        }
+    })
+    .catch((error)=>{
+        console.log("It Failed",error);
+    });
